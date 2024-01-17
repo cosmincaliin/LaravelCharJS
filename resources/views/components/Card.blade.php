5aff0542c32f4b3961ef06a633747c384b195af6
Cosmin Calin <gheorghe.cosmin.calin@gmail.com>
2024-01-17 17:42:38 +0100

    update Card Structure

--- resources/views/components/Card.blade.php
+++ resources/views/components/Card.blade.php
@@ -3,7 +3,7 @@
 <div class="relative">
     <img src="{{ $image }}" alt="Random Image" class="w-full h-64 object-cover mb-4">
     <div class="text-gray-900">
-        <div class="font-semibold text-xl mb-2">Fuente: {{ $source }}</div>
+        <div class="font-semibold text-xl mb-2">Sitio: {{ $source }}</div>
         <div class="group">
             <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @mouseover="showDetails = true" @mouseleave="showDetails = false">
                 View Details
